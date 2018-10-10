f13d9be2a54065ebfed7e8a34abe8044bc38e124
eveningstar123 <rarain1226@naver.com>
2018-10-10 18:03:59 +0900

    Add files via upload

--- ajax/text/data2018.js
+++ ajax/text/data2018.js
@@ -1,62 +1,62 @@
-[
-	{
-		"userId" : "유저1",
-		"listData" : "2018.10.01",
-		"contTxt" : "내용1"
-	},
-	{
-		"userId" : "유저2",
-		"listData" : "2018.10.02",
-		"contTxt" : "내용2"
-	},
-	{
-		"userId" : "유저3",
-		"listData" : "2018.10.03",
-		"contTxt" : "내용3"
-	},
-	{
-		"userId" : "유저4",
-		"listData" : "2018.10.04",
-		"contTxt" : "내용1"
-	},
-	{
-		"userId" : "유저5",
-		"listData" : "2018.10.05",
-		"contTxt" : "내용5"
-	},
-	{
-		"userId" : "유저6",
-		"listData" : "2018.10.06",
-		"contTxt" : "내용6"
-	},
-	{
-		"userId" : "유저7",
-		"listData" : "2018.10.07",
-		"contTxt" : "내용7"
-	},
-	{
-		"userId" : "유저8",
-		"listData" : "2018.10.08",
-		"contTxt" : "내용8"
-	},
-	{
-		"userId" : "유저9",
-		"listData" : "2018.10.09",
-		"contTxt" : "내용9"
-	},
-	{
-		"userId" : "유저10",
-		"listData" : "2018.10.10",
-		"contTxt" : "내용10"
-	},
-	{
-		"userId" : "유저11",
-		"listData" : "2018.10.11",
-		"contTxt" : "내용11"
-	},
-	{
-		"userId" : "유저12",
-		"listData" : "2018.10.12",
-		"contTxt" : "내용12"
-	}
-]
+var data2018 = [
+				{
+					"userId" : "유저1",
+					"listData" : "2018.10.01",
+					"contTxt" : "내용1"
+				},
+				{
+					"userId" : "유저2",
+					"listData" : "2018.10.02",
+					"contTxt" : "내용2"
+				},
+				{
+					"userId" : "유저3",
+					"listData" : "2018.10.03",
+					"contTxt" : "내용3"
+				},
+				{
+					"userId" : "유저4",
+					"listData" : "2018.10.04",
+					"contTxt" : "내용1"
+				},
+				{
+					"userId" : "유저5",
+					"listData" : "2018.10.05",
+					"contTxt" : "내용5"
+				},
+				{
+					"userId" : "유저6",
+					"listData" : "2018.10.06",
+					"contTxt" : "내용6"
+				},
+				{
+					"userId" : "유저7",
+					"listData" : "2018.10.07",
+					"contTxt" : "내용7"
+				},
+				{
+					"userId" : "유저8",
+					"listData" : "2018.10.08",
+					"contTxt" : "내용8"
+				},
+				{
+					"userId" : "유저9",
+					"listData" : "2018.10.09",
+					"contTxt" : "내용9"
+				},
+				{
+					"userId" : "유저10",
+					"listData" : "2018.10.10",
+					"contTxt" : "내용10"
+				},
+				{
+					"userId" : "유저11",
+					"listData" : "2018.10.11",
+					"contTxt" : "내용11"
+				},
+				{
+					"userId" : "유저12",
+					"listData" : "2018.10.12",
+					"contTxt" : "내용12"
+				}
+			]
